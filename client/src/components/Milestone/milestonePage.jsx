d8ccbc167c5b69ea8030fb591d3989bf82d6554e
Kenny Le <kennyle37@gmail.com>
2018-06-23 11:02:32 -0700

    Add barebone layout for Login and milestonePage

---
+++ client/src/components/Milestone/milestonePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class MilestonePage extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        This page will house all of the milestones component
+      </div>
+    )
+  }
+}
