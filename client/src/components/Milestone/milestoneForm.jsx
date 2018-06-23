1e47aec5d822b0033308ee989d4b0009f3639e61
Kenny Le <kennyle37@gmail.com>
2018-06-23 11:33:57 -0700

    Add all layouts for milestone page

---
+++ client/src/components/Milestone/milestoneForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class MilestoneForm extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        This component will render the form for user to fill out
+      </div>
+    )
+  }
+  
+}
--- client/src/components/Milestone/techStackListView.jsx
+++ client/src/components/Milestone/techStackListView.jsx
@@ -10,7 +10,7 @@ export default class TechStackListView extends React.Component {
     return (
       <div>
         This page will take data passed in from techStack.jsx and render them!
-      <div>
+      </div>
     )
   }
 }
