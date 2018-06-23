0122bed18ab22f2e1460619d0f85b355222ae18a
Kenny Le <kennyle37@gmail.com>
2018-06-23 11:49:08 -0700

    Added outline for application history page

---
+++ client/src/components/Application_History/ApplicationHistoryForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class ApplicationHistoryForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        This page will render a form for user to fill out their application history.
+      </div>
+    )
+  }
+}
---
+++ client/src/components/Application_History/ApplicationHistoryPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class ApplicationHistoryPage extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        This class will render all of our Application History page.
+      </div>
+    )
+  }
+}
