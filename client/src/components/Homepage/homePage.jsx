9096369c77d88bc5e12c954889b50e2e87ee88d5
Kenny Le <kennyle37@gmail.com>
2018-06-23 17:11:23 -0700

    Remove techStacks since we will no longer be using it Add outline code to to homePage

---
+++ client/src/components/Homepage/homePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class HomePage extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        This page will render the entire homepage!
+      </div>
+    )
+  }
+}
