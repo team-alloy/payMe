c2f3d7e92978042136c74612cb39b6eda681923b
Kenny Le <kennyle37@gmail.com>
2018-06-23 17:19:06 -0700

    Add outline for our component's userCard

---
+++ client/src/components/Homepage/userCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class UserCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        This component will render our user card
+      </div>
+    )
+  }
+}
---
+++ client/src/components/Homepage/userCardForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class UserCardForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        This component will render our UserCardForm
+      </div>
+    )
+  }
+}
