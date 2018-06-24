38305f278866a06c2a7792f738552fcbcf53acbf
Kenny Le <kennyle37@gmail.com>
2018-06-23 17:15:03 -0700

    Add outline to our reminder page and reminder views

---
+++ client/src/components/Homepage/reminder.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Reminder extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        This component will pass down all of our reminders
+      </div>
+    )
+  }
+}
