956faadf93fc7fdcefd570b5aaba4719dcdcf171
Kenny Le <kennyle37@gmail.com>
2018-06-25 12:01:00 -0700

    Refactor App component with React Router to render navBar Fix syntax error in Login.jsx Rename Tips component to TipsListContainer to specify component's purpose Import tipsListContainer inside tipsPage component Refactor index.jsx for readability Create navBar header to link components' path Create routes for components' path

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
-}
+  }
+  
+  // this function will handle our login form
+  // if successful, route the user to the home page
+  // if unsuccessful, render an error message
 
-render() {
-  return (
-    <div>
-      My Login Page
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        My Login Page
+      </div>
+    )
+  };
 }
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
+import {HomePage} from './Homepage/homePage.jsx';
+import {MilestonePage} from './Milestone/milestonePage.jsx';
+import {TipsPage} from './Tips/tipsPage.jsx';
+import {ApplicationHistoryPage} from './Application_History/ApplicationHistoryPage.jsx';
+import {Login} from './Login/login.jsx';
 
 export default class NavBar extends React.Component {
   constructor(props) {
     super(props);
   }
+  
+  // const Main = () => { 
+  //   <main>
+  //     <Switch>
+  //       <Route exact path='/' component={HomePage} />
+  //       <Route path='/api/milestone' component={MilestonePage} />
+  //       <Route path='/api/tips' component={TipsPage} />
+  //       <Route path='/api/applications' component={ApplicationHistoryPage} />
+  //       <Route path='/api/login' component={Login}/>
+  //     </Switch>
+  //   </main>
+  // }
 
   render() {
     return (
       <div>
-        This is navbar
+        <header>
+          <nav>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/api/milstones'>Milestones</Link></li>
+              <li><Link to='/api/tips'>Interview Tips</Link></li>
+              <li><Link to='/api/applications'>Application History</Link></li>
+              <li><Link to='/api/login'>Logout</Link></li>
+            </ul>
+          </nav>
+        </header>
+
+        <main>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/api/milestones' component={MilestonePage} />
+            <Route path='/api/tips' component={TipsPage} />
+            <Route path='/api/applications' component={ApplicationHistoryPage} />
+            <Route path='/api/login' component={Login}/>
+          </Switch>
+        </main>
       </div>
     )
   }
