7749c49d9e6f2710eb0f63e381133d3d44154c63
romcar <rommel.carrasquilla@gmail.com>
2018-06-25 11:37:41 -0400

    Remove query checking code, moved and refactored
      This code was moved to the /signup route and refactored to check
      object for keys.

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -8,22 +8,7 @@ module.exports = {
     return db.knex.select().from('users').where(query);
   },
   signUpNewUser: (userInfo) => {
-    console.log(userInfo, 'woot');
-    let query = {};
-
-    // make a query object with what values are passed in.
-    userInfo.first_name ? query.first_name = userInfo.first_name : undefined;
-    userInfo.last_name ? query.last_name = userInfo.last_name : undefined;
-    userInfo.email ? query.email = userInfo.email : undefined;
-    userInfo.username ? query.username = userInfo.username : undefined;
-    // bcrypt this hash
-    userInfo.hash ? query.hash = userInfo.hash : undefined;
-    userInfo.current_salary ? query.current_salary = userInfo.current_salary : undefined;
-
-    console.log(query);
     return db.knex('users')
-      .insert(query).catch(err => {
-          throw err;
-      });
+      .insert(userInfo);
   }
 }; //end exports
