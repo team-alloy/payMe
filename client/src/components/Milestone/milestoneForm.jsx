be86ed86b7b311d54a7602b918fe2159d6a3a649
Kenny Le <kennyle37@gmail.com>
2018-06-26 17:03:51 -0700

    Add a form to allow user to input their previous work experience and achievements!

--- client/src/components/Milestone/milestoneForm.jsx
+++ client/src/components/Milestone/milestoneForm.jsx
@@ -6,80 +6,112 @@ export default class MilestoneForm extends React.Component {
     super(props);
   }
 
+  companyDisplay() {
+    return (
+      <div className="previous-company-fill-in">
+        <label>Previous Experience</label>
+          <div className="employment-history-fill-in">
+            <label>Company</label>
+              <div className="field">
+            <input 
+              type="text" 
+              name="milestone[employment-history]" 
+              placeholder="Company" 
+            />
+              </div>
+            <label>Position's Title</label>
+              <div className="field">
+            <input 
+              type="text" 
+              name="milstone[position-history]" 
+              placeholder="Position's Title" 
+            />
+              </div>
+          </div>
+      </div>
+    )
+  }
+
+  jobDescriptionDisplay() {
+    return (
+      <div className="job-description-fill-in">
+        <label>Job Description</label>
+          <div className="field">
+            <textarea 
+              rows="4" 
+              placeholder="Please provide a brief description regarding your responsibilities."
+            ></textarea>
+          </div>
+      </div>  
+    )
+  }
+
+  techStackDisplay() {
+    return (
+      <div className="tech-stack-fill-in">
+        <label>Tech Stack</label>
+          <div className="field">
+            <textarea 
+                rows="4" 
+                placeholder="Please enter the tech stack used at your previous company, separated by commas."
+            ></textarea>    
+          </div>  
+      </div>
+    )
+  }
+
+  keyAccomplishmentsDisplay() {
+    return (
+      <div className="achievements-fill-in">
+        <label>Key Accomplishments</label>
+          <div className="inline-fields">
+            <label>Job Accomplishment: </label>
+            <div className="field">
+              <textarea 
+                rows="4"
+                placeholder="Please enter your key achievements here i.e. Implemented algorithm that improved company's efficiency by 50%!" 
+              ></textarea>
+            </div>
+            <label>Accomplishment's Result: </label>
+            <div className="field">
+              <textarea 
+                rows="4"
+                placeholder="Please enter the quantifiable result of your achievements here i.e. Saved the company over $100 million USD and was promoted to Chief Technology Officer." 
+              ></textarea>
+            </div>
+          </div>
+      </div>
+    )
+  }
+
+  repositoryDisplay() {
+    return (
+      <div className="repository-fill-in">
+        <label>Repository Link</label>
+          <div className="field">
+            <textarea 
+              rows="1" 
+              placeholder="Please enter your repository link here."
+            ></textarea>
+          </div>
+      </div>
+    )
+  }
+
   milestoneDisplayForm() {
     return (
       <div>
         <form className="ui-form">
           <h4 className="ui-title-header">Career Milestones and Achievements</h4>
           <div className="employment-history-field">
-            <label>Previous Experience</label>
-            <div className="employment-history-fill-in">
-              <label>Company</label>
-              <div className="field">
-                <input 
-                  type="text" 
-                  name="milestone[employment-history]" 
-                  placeholder="Company" 
-                />
-              </div>
-              <label>Position's Title</label>
-              <div className="field">
-                <input 
-                  type="text" 
-                  name="milstone[position-history]" 
-                  placeholder="Position's Title" 
-                />
-              </div>
-            </div>
-
-            <div className="job-description-fill-in">
-              <label>Job Description</label>
-                <div className="field">
-                  <textarea 
-                    rows="4" 
-                    placeholder="Please provide a brief description regarding your previous's work responsibilities."
-                  ></textarea>
-                </div>
-            </div>
-
-            <div className="tech-stack-fill-in">
-              <label>Tech Stack</label>
-                <div className="field">
-                  <textarea 
-                      rows="4" 
-                      placeholder="Please enter the tech stack used at your previous company, separated by commas."
-                    ></textarea>
-                </div>
-            </div>
-
-            <div className="achievements-fill-in">
-              <label>Key Accomplishments</label>
-              <div className="inline-fields">
-                <label>Job Accomplishment: </label>
-                <div className="field">
-                  <input 
-                    type="text" 
-                    placeholder="Please enter your key achievements here i.e. Implemented algorithm that improved company's efficiency by 50%!" 
-                  />
-                </div>
-                <label>Accomplishment's Result: </label>
-                <div className="field">
-                  <input 
-                    type="text" 
-                    placeholder="Please enter the quantifiable result of your achievements here i.e. Saved the company over $100 million USD and was promoted to Chief Technology Officer." 
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="repository-fill-in">
-              <label>Repository Link</label>
-                <div className="field">
-                  <textarea 
-                    rows="1" 
-                    placeholder="Please enter your repository link here."
-                  ></textarea>
-                </div>
+            {this.companyDisplay()}
+            {this.jobDescriptionDisplay()}
+            {this.techStackDisplay()}
+            {this.keyAccomplishmentsDisplay()}
+            {this.repositoryDisplay()}
+            <div className="button-container">
+              <button className="ui-button-confirm" tabIndex="0">Confirm</button>
+              <button className="ui-button-cancel" tabIndex="1">Cancel</button>
             </div>
           </div>
         </form>
