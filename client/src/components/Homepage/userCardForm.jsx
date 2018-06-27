5584d6af8f796dd53c5d1f1a0ecc01a80051c89c
Kenny Le <kennyle37@gmail.com>
2018-06-27 14:37:25 -0700

    Added display screen for home page, allowing user to view their own profile

--- client/src/components/Homepage/userCardForm.jsx
+++ client/src/components/Homepage/userCardForm.jsx
@@ -67,6 +67,11 @@ export default class UserCardForm extends React.Component {
             </div>
           </div>
 
+        <div className="button-container">
+          <button className="ui-button-cancel" tabIndex="0">Cancel</button>
+          <button className="ui-button-confirm" tabIndex="1">Confirm</button>
+        </div>
+        
         </form>
       </div>
     )
@@ -75,11 +80,8 @@ export default class UserCardForm extends React.Component {
   render() {
     return (
       <div>
-        {this.userFormDisplay()}
-        <div className="button-container">
-          <button className="ui-button-cancel" tabIndex="0">Cancel</button>
-          <button className="ui-button-confirm" tabIndex="1">Confirm</button>
-        </div>
+        {/* {this.userFormDisplay()} */}
+        {this.currentDisplay()}
       </div>
     )
   }
