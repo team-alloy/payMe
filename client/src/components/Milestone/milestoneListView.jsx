9e5daa67d6f1f2abe30fd1c8d26a0a12b5883b8b
Kenny Le <kennyle37@gmail.com>
2018-06-26 20:00:23 -0700

    Add basic rendering for milestone display

--- client/src/components/Milestone/milestoneListView.jsx
+++ client/src/components/Milestone/milestoneListView.jsx
@@ -6,23 +6,94 @@ export default class MilestoneListView extends React.Component {
   }
   
   // this function will render our read-only form
-    //company
-      //position title
-      //job description
-      //techstack
-    //key accomplishments
-      //job accomplishments
-      //accomplishment's result
+    //previous projects
+      //project's name
+        //project description
+      //tech stack
+        //tech stack list
       //repository link
+        //repositorylink
+      //date
+        //date
 
-  // milestoneDisplayView() {
+  projectDisplayView() {
+    return (
+      <div>
+        <div className="name-header">
+          <h5>Project's Name</h5>
+        </div>
+        <div className="project-display-description-container">
+          <div className="project-display-description">
+            This application helps user negotiate their salary!
+          </div>
+        </div>
+      </div>
+    )
+  }
 
-  // }
+  techStackDisplayView() {
+    return (
+      <div>
+        <div className="name-header">
+          <h5>Tech Stack</h5>
+        </div>
+        <div className="stack-display-description-container">
+          <div className="stack-display-description">
+            JavaScript, ReactJS, mySQL, SemanticUI, Google.
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  repositoryLinkDisplay() {
+    return (
+      <div>
+        <div className="name-header">
+          <h5>Repository</h5>
+        </div>
+        <div className="respository-display-description-container">
+          <div className="repository-display-description">
+            www.github.com/SirGitchardHubbard
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  dateDisplay() {
+    return (
+      <div>
+        <div className="name-header">
+          <h5>Completed On: </h5>
+        </div>
+        <div className="date-display-container">
+          <span className="date-display">
+            1/1/2018
+          </span>
+        </div>
+      </div>
+    )
+  }
+
+  milestoneDisplayView() {
+    return (
+      <div>
+        <div className="title-header">
+          <h4>Milestones and Achievements</h4>
+        </div>
+        {this.projectDisplayView()}
+        {this.techStackDisplayView()}
+        {this.repositoryLinkDisplay()}
+        {this.dateDisplay()}
+      </div>
+    )
+  }
 
   render() {
     return (
       <div>
-
+        {this.milestoneDisplayView()}
       </div>
     )
   }
