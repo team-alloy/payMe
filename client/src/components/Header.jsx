3273c7a160b04763b6821840185c3351b708ca12
Mealear <mileyy.mk@gmail.com>
2018-06-28 16:35:44 -0700

    Lint components Header and Main

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,37 +2,51 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Header extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
   render() {
     return (
       <div className="ui top grey inverted seven item menu">
         <div className="item">
-        <Link to="/"><img src="/images/logo.png" style={{width: 75, height: 75}}/></Link>
+          <Link to="/">
+             <img src="/images/logo.png" style={{width: 75, height: 75}}/>
+          </Link>
         </div>
         <div className="item">
-            <Link to="/">Home</Link>
+          <Link to="/">
+            Home
+          </Link>
         </div>
         <div className="item">
-            <Link to="/milestones">Milestones</Link>
+          <Link to="/milestones">
+            Milestones
+          </Link>
         </div>
         <div className="item">
-            <Link to="/tips">Negotiation Tips</Link>
+          <Link to="/tips">
+             Negotiation Tips
+          </Link>
         </div>
         <div className="item">
-            <Link to="/applications">Job Application History</Link>
+          <Link to="/applications">
+            Job Application History
+          </Link>
         </div>
         <div className="item">
-            <Link to="/practice">Negotiation Practice</Link>
+          <Link to="/practice">
+            Negotiation Practice
+          </Link>
         </div>
         <div className="item">
-            <Link to="/login">Logout</Link>
+          <Link to="/login">
+            Logout
+          </Link>
         </div>
       </div>
-    )
+    );
   }
-};
-    
-
+}
