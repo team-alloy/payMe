da0d512538e0d5a39586c8b4124cd371e11a1c5a
Mealear <mileyy.mk@gmail.com>
2018-06-28 16:57:57 -0700

    Lint Login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,49 +1,59 @@
 import React from 'react';
-import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
+import {
+  Button, Form, Grid, Header, Message, Segment,
+} from 'semantic-ui-react';
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+    };
   }
-  
+
   render() {
     return (
       <div>
-        <div className='login-form'>
-          <style>{`
+        <div className="login-form">
+          <style>
+            {`
             body > div,
             body > div > div,
             body > div > div > div.login-form {
               height: 100%;
             }
-          `}</style>
-          <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+            `}
+          </style>
+          <Grid textAlign="center" style={{ height: '100%' }} verticalAlign="middle">
             <Grid.Column style={{ maxWidth: 450 }}>
-              <Header as='h2' color='teal' textAlign='center'>
+              <Header as="h2" color="teal" textAlign="center">
                 Please log in!
               </Header>
-              <Form size='large'>
+              <Form size="large">
                 <Segment stacked>
-                  <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                  <Form.Input fluid icon="user" iconPosition="left" placeholder="E-mail address" />
                   <Form.Input
                     fluid
-                    icon='lock'
-                    iconPosition='left'
-                    placeholder='Password'
-                    type='password'
+                    icon="lock"
+                    iconPosition="left"
+                    placeholder="Password"
+                    type="password"
                   />
-                  <Button color='teal' fluid size='large'>
+                  <Button color="teal" fluid size="large">
                     Login
                   </Button>
                 </Segment>
               </Form>
               <Message>
-                New User? <a href='/signup'>Sign Up to Get That Bonus!</a>
+                New User?
+                {' '}
+                <a href="/signup">
+                   Sign Up to Get That Bonus!
+                </a>
               </Message>
             </Grid.Column>
           </Grid>
         </div>
       </div>
-    )
-  };
+    );
+  }
 }
