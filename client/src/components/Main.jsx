af5407b39cb4490c9eeb8948ad9f3b0488f38a62
Mealear <mileyy.mk@gmail.com>
2018-06-28 15:53:42 -0700

    Installed eslint and linted components Main, SignUp, and Navbar

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import HomePage from './Homepage/HomePage';
-import MilestonePage from './Milestone/MilestonePage';
-import TipsPage from './Tips/TipsPage';
-import ApplicationHistoryPage from './Application_History/ApplicationHistoryPage';
-import Login from './Login/Login';
-import SignUp from './Signup/Signup';
-import NegotiationPracticePage from './Negotiation_Practice/NegotiationPracticePage';
+import HomePage from './Homepage/HomePage.jsx';
+import MilestonePage from './Milestone/MilestonePage.jsx';
+import TipsPage from './Tips/TipsPage.jsx';
+import ApplicationHistoryPage from './Application_History/ApplicationHistoryPage.jsx';
+import Login from './Login/Login.jsx';
+import SignUp from './Signup/Signup.jsx';
+import NegotiationPracticePage from './Negotiation_Practice/NegotiationPracticePage.jsx';
 
 
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+
+    };
   }
 
   render() {
     return (
       <main>
         <Switch>
-          <Route exact path='/' render={(props) => {
-            return <HomePage {...props} />
-          }} />
-          <Route path='/milestones' component={MilestonePage} />
-          <Route path='/tips' component={TipsPage} />
-          <Route path='/applications' component={ApplicationHistoryPage} />
-          <Route path='/practice' component={NegotiationPracticePage} />
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={SignUp} />
+          <Route
+            exact
+            path="/"
+            render={props => <HomePage {...props} />}
+          />
+          <Route path="/milestones" component={MilestonePage} />
+          <Route path="/tips" component={TipsPage} />
+          <Route path="/applications" component={ApplicationHistoryPage} />
+          <Route path="/practice" component={NegotiationPracticePage} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
         </Switch>
       </main>
-    )
+    );
   }
-};
+}
--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -3,56 +3,77 @@ import React from 'react';
 export default class SignUp extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+
+    };
   }
 
   render() {
     return (
       <div className="ui column stackable center page grid">
-        <h2>Register</h2>
-        <h5>...to get that raise you deserve</h5>
-        <br></br>
+        <h2>
+           Register
+        </h2>
+        <h5>
+          ...to get that raise you deserve
+        </h5>
+        <br />
         <form className="ui form">
 
           <div className="fields">
             <div className="field">
-              <label>First Name</label>
+              <label>
+                First Name
+              </label>
               <input type="text" name="first-name" placeholder="First Name" />
             </div>
             <div className="field">
-              <label>Last Name</label>
+              <label>
+                Last Name
+              </label>
               <input type="text" name="last-name" placeholder="Last Name" />
             </div>
           </div>
 
           <div className="fields">
             <div className="field">
-              <label>Email Address</label>
+              <label>
+                Email Address
+              </label>
               <input type="text" name="email" placeholder="Email Address" />
             </div>
             <div className="field">
-              <label>Username</label>
+              <label>
+                Username
+              </label>
               <input type="text" name="username" placeholder="Username" />
             </div>
           </div>
 
           <div className="fields">
             <div className="field">
-              <label>Password</label>
+              <label>
+                Password
+              </label>
               <input type="text" name="password" placeholder="Password" />
             </div>
             <div className="field">
-              <label>Re-enter Password</label>
+              <label>
+                Re-enter Password
+              </label>
               <input type="text" name="re-enter-pw" placeholder="Re-enter Password" />
             </div>
           </div>
 
-          <br></br>
+          <br />
 
           <button className="small teal basic ui button" tabIndex="0" type="submit">
-            <a href='/login'>Login With Your New Account</a>
+            <a href="/login">
+              Login With Your New Account
+            </a>
           </button>
         </form>
       </div>
-    )
+    );
   }
-};
+}
