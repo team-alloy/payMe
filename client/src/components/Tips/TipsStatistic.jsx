398282d2e45d0a7fc324ff93f55b4fe0c6a0d711
Mealear <mileyy.mk@gmail.com>
2018-06-28 16:51:45 -0700

    Lint TipsStatistic

--- client/src/components/Tips/TipsStatistic.jsx
+++ client/src/components/Tips/TipsStatistic.jsx
@@ -3,18 +3,31 @@ import React from 'react';
 export default class TipsStatistic extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+    };
   }
 
   render() {
     return (
       <div>
-        According to Glassdoor, the average Software Engineer salary in the U.S. makes an average salary of $92,790.
-        <br></br>
-        Your job as a <b>Software Engineer</b> in <b>San Francisco, CA</b> has an average market value of:
-        <b>$111,885.</b>
-        <br></br>
+        According to Glassdoor, the average Software Engineer salary in the U.S.
+        makes an average salary of $92,790.
+        <br />
+        Your job as a
+        <b>
+          Software Engineer
+        </b>
+        in
+        <b>
+          San Francisco, CA
+        </b>
+        has an average market value of:
+        <b>
+          $111,885.
+        </b>
+        <br />
         Based on this data, here is what we suggest:
       </div>
-    )
+    );
   }
-};
+}
