caf54364997642928acef16712747ab02410611c
romcar <rommel.carrasquilla@gmail.com>
2018-06-28 18:06:46 -0400

    Add resolver to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,27 @@
 var path = require('path');
-var SRC = path.join(__dirname, '/client/src');
-var DEST = path.join(__dirname, '/client/dist');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DEST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: `${SRC}/index.jsx`,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DEST,
+    path: DEST_DIR,
   },
   module: {
     rules: [
       {
+        test: /\.jsx?/,
+        include: SRC_DIR,
         exclude: ['node_modules'],
         loader: 'babel-loader',
-        test: /\.jsx?$/
-      },
-      {
-        test: /\.jsx?/,
-        include: SRC,
-        loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015', 'stage-1']
+          presets: ['react', 'env']
         }
       }
     ]
+  },
+  resolve: {
+    extensions: ['.js','.jsx','.json']
   }
 };
