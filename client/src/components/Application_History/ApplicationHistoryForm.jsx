c6119ef3d5634a5bffef999b8e2b900b9a54acb4
Kenny Le <kennyle37@gmail.com>
2018-06-28 20:34:51 -0700

    Refactor ApplicationHistoryForm and ApplicationHistoryPage to conform with eSLINT

--- client/src/components/Application_History/ApplicationHistoryForm.jsx
+++ client/src/components/Application_History/ApplicationHistoryForm.jsx
@@ -11,27 +11,39 @@ export default class ApplicationHistoryForm extends React.Component {
   render() {
     return (
       <div>
-        <h4>Application Information:</h4>
+        <h4>
+          Application Information:
+        </h4>
         <form className="ui-form">
-          <label>Company's Name</label>
+          <label htmlFor="name">
+            Company's Name
+          </label>
           <div className="app-field">
-            <textarea className="company-name" placeholder="Company's Name"></textarea>
+            <textarea className="company-name" placeholder="Company's Name" />
           </div>
-          <label>Position's Title</label>
+          <label htmlFor="position">
+            Position's Title
+          </label>
           <div className="app-field">
-            <textarea className="position-name" placeholder="Position's Title"></textarea>
+            <textarea className="position-name" placeholder="Position's Title" />
           </div>
-          <label>Company's Location</label>
+          <label htmlFor="location">
+            Company's Location
+          </label>
           <div className="app-field">
-            <textarea className="location-name" placeholder="Company's Location"></textarea>
+            <textarea className="location-name" placeholder="Company's Location" />
           </div>
-          <label>Date Applied</label>
+          <label htmlFor="date">
+            Date Applied
+          </label>
           <div className="app-field">
-            <input className="date-name" type="date"></input>
+            <input className="date-name" type="date" />
           </div>
         </form>
         <div className="button-container">
-          <button className="ui-button-cancel" type="reset">Cancel</button>
+          <button className="ui-button-cancel" type="reset">
+            Cancel
+          </button>
           <button className="ui-button-confirm" type="submit">
             Confirm
           </button>
