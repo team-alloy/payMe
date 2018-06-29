605058c94977b82d166aedd2cb4b37ba70d05235
Mealear <mileyy.mk@gmail.com>
2018-06-28 17:00:03 -0700

    Lint Negotiation components

--- client/src/components/Negotiation_Practice/NegotiationPracticePage.jsx
+++ client/src/components/Negotiation_Practice/NegotiationPracticePage.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import NegotiationPracticeVideo from './NegotiationPracticeVideo.jsx';
-import NegotiationPracticeTopic from './NegotiationPracticeTopic.jsx';
+import NegotiationPracticeVideo from './NegotiationPracticeVideo';
+import NegotiationPracticeTopic from './NegotiationPracticeTopic';
 
 export default class NegotiationPracticePage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+    };
   }
 
   render() {
     return (
       <div className="ui column stackable center page grid">
-        <NegotiationPracticeVideo/>
-        <NegotiationPracticeTopic/>
+        <NegotiationPracticeVideo />
+        <NegotiationPracticeTopic />
       </div>
-    )
+    );
   }
-};
+}
--- client/src/components/Negotiation_Practice/NegotiationPracticeTopic.jsx
+++ client/src/components/Negotiation_Practice/NegotiationPracticeTopic.jsx
@@ -3,17 +3,23 @@ import React from 'react';
 export default class NegotiationPracticeTopic extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+    };
   }
 
   render() {
     return (
       <div>
-        This is your negotiation topic of the day: 
-        <ul><li>Paid Time Off</li></ul>
-        Your partner who will interview you is: 
+        This is your negotiation topic of the day:
+        <ul>
+          <li>
+            Paid Time Off
+          </li>
+        </ul>
+        Your partner who will interview you is:
         Kenny Le,
         Software Engineer at Google
       </div>
-    )
+    );
   }
-};
+}
