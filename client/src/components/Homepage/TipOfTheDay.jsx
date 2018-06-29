966585bea8314380b5818ce5244f32387fe76e4b
Kenny Le <kennyle37@gmail.com>
2018-06-28 19:30:23 -0700

    Refactor TipOfTheDay and UserCard to conform to ESLINT Add ESLint form handler setting

--- client/src/components/Homepage/TipOfTheDay.jsx
+++ client/src/components/Homepage/TipOfTheDay.jsx
@@ -3,28 +3,29 @@ import React from 'react';
 export default class TipOfTheDay extends React.Component {
   constructor(props) {
     super(props);
-  }
+    this.state = {
 
-  TipOfTheDayDisplay() {
-    return (
-      <table class="ui celled padded table">
-        <thead>
-          <th>Tip of the Day</th>
-        </thead>
-        <tbody>
-          <div className="tip-container">
-            <td>Getting a good night sleep is a key to a good interview!</td>
-          </div>
-        </tbody>
-      </table>
-    )
+    };
   }
 
   render() {
     return (
       <div>
-        {this.TipOfTheDayDisplay()}
+        <table className="ui celled padded table">
+          <thead>
+            <th>
+              Tip of the Day
+            </th>
+          </thead>
+          <tbody>
+            <div className="tip-container">
+              <td>
+                Getting a good night sleep is a key to a good interview!
+              </td>
+            </div>
+          </tbody>
+        </table>
       </div>
-    )
+    );
   }
 }
