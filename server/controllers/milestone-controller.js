da7202b3cdba07a995f6e2819734945a788bc52f
romcar <rommel.carrasquilla@gmail.com>
2018-06-30 01:50:55 -0400

    Lint 4 controllers - application - company - milestone - offer

--- server/controllers/milestone-controller.js
+++ server/controllers/milestone-controller.js
@@ -1,15 +1,9 @@
 const db = require('../../database/index.js');
 
 module.exports = {
-  findAllMilestones : () => {
-    return db.knex.select().from('milestones');
-  },
-  insertMilestone : (milestone) => {
-    return db.knex('milestones').insert(milestone);
-  },
-  updateMilestone : (id, update) => {
-    return db.knex('milestones')
+  findAllMilestones: () => db.knex.select().from('milestones'),
+  insertMilestone: milestone => db.knex('milestones').insert(milestone),
+  updateMilestone: (id, update) => db.knex('milestones')
     .where('id', id)
-    .update(update);
-  }
-}
+    .update(update),
+};
