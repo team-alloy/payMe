09d6b99c1517b6bbbec2ec3ab7cf876da1ef0c51
Kenny Le <kennyle37@gmail.com>
2018-06-30 14:36:17 -0700

    Implementing userCardForm switch

--- client/src/components/Homepage/HomePage.jsx
+++ client/src/components/Homepage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Segment } from 'semantic-ui-react';
-import UserCardForm from './UserCardForm';
+import UserCard from './UserCardForm';
 import TipOfTheDay from './TipOfTheDay';
 import Reminder from './Reminder';
 
@@ -14,6 +14,7 @@ class HomePage extends React.Component {
   }
 
   render() {
+    // console.log(this.props)
     if (!this.props.session.user) {
       this.props.history.push('/login');
       return (
