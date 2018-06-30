5364c56fda8b98dedbec4075735055562862f0e4
Kenny Le <kennyle37@gmail.com>
2018-06-30 16:02:19 -0700

    Refactor UserCardForm to prepare for Editable Form

--- client/src/components/Homepage/UserCardForm.jsx
+++ client/src/components/Homepage/UserCardForm.jsx
@@ -13,43 +13,46 @@ export default class UserCardForm extends React.Component {
   render() {
     return (
       <div className="ui teal card">
-        <a style={{fontWeight: 'bold'}}>Edit User's Information</a>
+        <h4 class="ui dividing header left aligned segment">Edit User's Profile</h4>
         <form className="ui-form">
+          <div className="field">
+            <div className="left aligned segment" style={{ fontWeight: 'bold' }}>
+              Full Name
+            </div>
+            <div className="inline fields">
+              <div className="field">
+                <input type="text" placeholder="First Name"></input>
+              </div>
+              <div className="field">
+                <input type="text" placeholder="Last Name"></input>
+              </div>
+            </div>
+          </div>
 
-          <div className="inline-field">
-            <label style={{fontWeight: 'bold'}}>Full Name</label>
+          <div className="field">
+            <label style={{fontWeight: 'bold'}}>
+              {'Position\'s Title: '} 
+            </label>
             <div className="field">
-              <input type="text" placeholder="First Name"></input>
+              <input type="text"></input>
             </div>
+          </div>
+
+          <div className="field">
+            <label style={{fontWeight: 'bold'}}>
+              {'Employer: '}
+            </label>
             <div className="field">
-              <input type="text" placeholder="Last Name"></input>
+              <input type="text" />
             </div>
           </div>
 
-          <div className="inline-field">
-            <div className="position-field">
-              <label style={{fontWeight: 'bold'}}>Position's Title: </label>
-              <div className="field">
-                <input type="text"></input>
-              </div>
-            </div>
-          </div>
-
-          <div className="inline-field">
-            <div className="employer-field">
-              <label style={{fontWeight: 'bold'}}>Employer: </label>
-              <div className="field">
-                <input type="text"></input>
-              </div>
-            </div>
-          </div>
-
-          <div className="inline-field">
-            <div className="salary-field">
-              <label style={{fontWeight: 'bold'}}>Salary: </label>
-              <div className="field">
-                <input type="text"></input>
-              </div>
+          <div className="field">
+            <label style={{fontWeight: 'bold'}}>
+              {'Salary: '} 
+            </label>
+            <div className="field">
+              <input type="text" />
             </div>
           </div>
 
