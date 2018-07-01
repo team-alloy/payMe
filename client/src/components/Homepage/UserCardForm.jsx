c87356aa87a678804959bf549bf91ab16027c2dd
Kenny Le <kennyle37@gmail.com>
2018-06-30 21:18:02 -0700

    Implement form handler for UserCardForm

--- client/src/components/Homepage/UserCardForm.jsx
+++ client/src/components/Homepage/UserCardForm.jsx
@@ -6,15 +6,30 @@ export default class UserCardForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: 'default',
+      name: '',
+      position: '',
+      employer: '',
+      salary: '',
     };
+
+    this.positionChange = this.positionChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  positionChange(event) {
+    this.setState({ position: event.target.value })
+  }
+
+  handleSubmit(event) {
+    console.log('submitted! ' + this.state.position);
+    event.preventDefault();
   }
 
   render() {
     return (
       <div className="ui teal card">
         <h4 className="ui dividing header left aligned segment">Edit User's Profile</h4>
-        <form className="ui-form">
+        <form className="ui-form" onSubmit={this.handleSubmit}>
           <div className="field">
             <div className="left aligned segment" style={{ fontWeight: 'bold' }}>
               Full Name
@@ -34,7 +49,7 @@ export default class UserCardForm extends React.Component {
               {'Position\'s Title: '} 
             </label>
             <div className="field">
-              <input type="text"></input>
+              <input type="text" value={this.state.position} onChange={this.positionChange}></input>
             </div>
           </div>
 
