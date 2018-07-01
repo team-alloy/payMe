2d4e2f443beae11739eb6f87f629e8aad4faaab4
Kenny Le <kennyle37@gmail.com>
2018-06-30 22:17:30 -0700

    User form now updates name

--- client/src/components/Homepage/UserCardForm.jsx
+++ client/src/components/Homepage/UserCardForm.jsx
@@ -1,28 +1,58 @@
 import React from 'react';
 import UserCard from './UserCard';
 import { connect } from 'react-redux';
+import { ENGINE_METHOD_CIPHERS } from 'constants';
 
 export default class UserCardForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       name: '',
+      firstName: '',
+      lastName: '',
       position: '',
       employer: '',
       salary: '',
     };
 
+    this.nameChange = this.nameChange.bind(this);
+    this.firstNameChange = this.firstNameChange.bind(this);
+    this.lastNameChange = this.lastNameChange.bind(this);
     this.positionChange = this.positionChange.bind(this);
+    this.employerChange = this.employerChange.bind(this);
+    this.salaryChange = this.salaryChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  nameChange() {
+    const { firstName } = this.state;
+    const { lastName } = this.state;
+    this.setState({ name: `${firstName} ${lastName}` });
+  }
+
+  firstNameChange(event) {
+    this.setState({ firstName: event.target.value });
+  }
+
+  lastNameChange(event) {
+    this.setState({ lastName: event.target.value });
+  }
+
   positionChange(event) {
-    this.setState({ position: event.target.value })
+    this.setState({ position: event.target.value });
+  }
+
+  employerChange(event) {
+    this.setState({ employer: event.target.value });
+  }
+
+  salaryChange(event) {
+    this.setState({ salary: event.target.value });
   }
 
   handleSubmit(event) {
-    console.log('submitted! ' + this.state.position);
     event.preventDefault();
+    this.nameChange();
   }
 
   render() {
@@ -36,10 +66,10 @@ export default class UserCardForm extends React.Component {
             </div>
             <div className="inline fields">
               <div className="field">
-                <input type="text" placeholder="First Name"></input>
+                <input type="text" placeholder="First Name" value={this.state.firstName} onChange={this.firstNameChange}></input>
               </div>
               <div className="field">
-                <input type="text" placeholder="Last Name"></input>
+                <input type="text" placeholder="Last Name" value={this.state.lastName} onChange={this.lastNameChange}></input>
               </div>
             </div>
           </div>
@@ -49,7 +79,7 @@ export default class UserCardForm extends React.Component {
               {'Position\'s Title: '} 
             </label>
             <div className="field">
-              <input type="text" value={this.state.position} onChange={this.positionChange}></input>
+              <input type="text" value={this.state.position} onChange={this.positionChange} />
             </div>
           </div>
 
@@ -58,7 +88,7 @@ export default class UserCardForm extends React.Component {
               {'Employer: '}
             </label>
             <div className="field">
-              <input type="text" />
+              <input type="text" value={this.state.employer} onChange={this.employerChange} />
             </div>
           </div>
 
@@ -67,15 +97,14 @@ export default class UserCardForm extends React.Component {
               {'Current Salary: '}
             </label>
             <div className="field">
-              <input type="text" />
+              <input type="text" value={this.state.salary} onChange={this.salaryChange} />
             </div>
           </div>
 
         <div className="button-container">
-          <button className="ui-button-cancel" tabIndex="0">Cancel</button>
-          <button className="ui-button-confirm" tabIndex="1">Confirm</button>
+          <button className="ui-button-cancel">Cancel</button>
+          <button className="ui-button-confirm">Confirm</button>
         </div>
-
         </form>
       </div>
     )
