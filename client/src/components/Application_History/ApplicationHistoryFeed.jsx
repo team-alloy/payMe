915d6a6adb12a0b91765c06203f7165d6a65be16
Erwin R. Carrasquilla <31927972+romcar@users.noreply.github.com>
2018-07-02 13:43:53 -0400

    Update ApplicationHistoryFeed.jsx

--- client/src/components/Application_History/ApplicationHistoryFeed.jsx
+++ client/src/components/Application_History/ApplicationHistoryFeed.jsx
@@ -10,7 +10,6 @@ export default class ApplicationHistoryFeed extends React.Component {
   }
 
   render() {
-    console.log(this.props,'histFeedREnder');
     if(this.props.apps) {
       return (
         <div>
