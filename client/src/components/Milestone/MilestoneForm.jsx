c4b32763a76b3838fd81d08ad47d23fdb6b6a892
Kenny Le <kennyle37@gmail.com>
2018-07-05 23:12:09 -0700

    Redesign Milestone Form

--- client/src/components/Milestone/MilestoneForm.jsx
+++ client/src/components/Milestone/MilestoneForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MilestonePage from './MilestonePage.jsx';
 import MilestoneListView from './MilestoneListView.jsx';
-import { Card } from 'semantic-ui-react';
+import { Button, Form } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import axios from 'axios';
@@ -67,99 +67,91 @@ export class MilestoneForm extends React.Component {
   render() {
     console.log(this.props)
     return (
-      <Card raised className="ui teal segment">
-        <form className="ui-form" onSubmit={this.handleSubmit}>
-          <h4 className="ui-title-header">
-            Career Milestones and Achievements
-          </h4>
-          <div className="employment-history-fill-in">
-            <label htmlFor="name">
-              <b>
-                {'Project\'s Name'}
-              </b>
-              <br />
-            </label>
-            <textarea
-              rows="1"
-              cols="32"
-              type="text"
-              name="milestone[project-history]"
-              placeholder="Enter Your Project's Name Here!"
-              value={this.state.name}
-              onChange={this.milestoneNameChange}
-            />
-          </div>
-          <div className="job-description-fill-in">
-            <label htmlFor="description">
-              <b>
-                {'Job Description'}
-              </b>
-              <br />
-            </label>
-            <textarea
-              rows="3"
-              cols="32"
-              placeholder="Please provide a brief description regarding your project."
-              value={this.state.description}
-              onChange={this.descriptionNameChange}
-            />
-          </div>
-          <div className="tech-stack-fill-in">
-            <label htmlFor="tech-stack">
-              <b>
-                {'Tech Stack(s)'}
-              </b>
-              <br />
-            </label>
-            <textarea
-              rows="4"
-              cols="32"
-              placeholder="Please enter the tech stack used at your previous company, separated by commas."
-              value={this.state.stack}
-              onChange={this.stackNameChange}
-            />
-          </div>
-          <div className="repository-fill-in">
-            <label htmlFor="respository">
-              <b>
-                {'Repository Link'}
-              </b>
-              <br />
-            </label>
-            <textarea
-              rows="1"
-              cols="32"
-              placeholder="Please enter your repository link here."
-              value={this.state.repositoryNameChange}
-              onChange={this.repositoryNameChange}
-            />
-          </div>
-          <div className="date-fill-in">
-            <label htmlFor="date">
-              <b>
-                {'Date Completed'}
-              </b>
-              <br />
-            </label>
-            <input
-              type="date"
-              rows="1"
-              cols="32"
-              placeholder="Please enter the date for this project."
-              value={this.state.completedDate}
-              onChange={this.completedDate}
-            />
-          </div>
-          <div className="button-container">
-            <button className="ui-button-cancel">
-              Cancel
-            </button>
-            <button className="ui-button-confirm" type="submit">
-              Confirm
-            </button>
-          </div>
-        </form>
-      </Card>
+      <Form raised className="ui teal segment" onSubmit={this.handleSubmit}>
+        <h4 className="ui center aligned segment">
+          Career Milestones and Achievements
+        </h4>
+        <div className="employment-history-fill-in">
+          <label htmlFor="name">
+            <b>
+              {'Project\'s Name'}
+            </b>
+            <br />
+          </label>
+          <textarea
+            rows="1"
+            type="text"
+            name="milestone[project-history]"
+            placeholder="Enter Your Project's Name Here!"
+            value={this.state.name}
+            onChange={this.milestoneNameChange}
+          />
+        </div>
+        <div className="job-description-fill-in">
+          <label htmlFor="description">
+            <b>
+              {'Job Description'}
+            </b>
+            <br />
+          </label>
+          <textarea
+            rows="2"
+            placeholder="Please provide a brief description regarding your project."
+            value={this.state.description}
+            onChange={this.descriptionNameChange}
+          />
+        </div>
+        <div className="tech-stack-fill-in">
+          <label htmlFor="tech-stack">
+            <b>
+              {'Tech Stack(s)'}
+            </b>
+            <br />
+          </label>
+          <textarea
+            rows="3"
+            placeholder="Please enter the tech stack used at your previous company, separated by commas."
+            value={this.state.stack}
+            onChange={this.stackNameChange}
+          />
+        </div>
+        <div className="repository-fill-in">
+          <label htmlFor="respository">
+            <b>
+              {'Repository Link'}
+            </b>
+            <br />
+          </label>
+          <textarea
+            rows="2"
+            placeholder="Please enter your repository link here."
+            value={this.state.repositoryNameChange}
+            onChange={this.repositoryNameChange}
+          />
+        </div>
+        <div className="date-fill-in">
+          <label htmlFor="date">
+            <b>
+              {'Date Completed'}
+            </b>
+            <br />
+          </label>
+          <input
+            type="date"
+            placeholder="Please enter the date for this project."
+            value={this.state.completedDate}
+            onChange={this.completedDate}
+          />
+        </div>
+        <div className="ui two bottom attached buttons">
+          <Button className="ui-button-cancel">
+            Cancel
+          </Button>
+          <Button className="ui-button-confirm" color="teal" type="submit" size="medium">
+            Confirm
+          </Button>
+        </div>
+      </Form>
     );
   }
 }
