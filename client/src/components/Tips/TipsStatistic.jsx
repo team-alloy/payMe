a48b7118fe09cbe2d96565a8064e28b1b543f712
romcar <rommel.carrasquilla@gmail.com>
2018-07-07 14:33:03 -0400

    Create statistical portion of the page,
    grab average salary and create a .07 +- range. To be displayed

--- client/src/components/Tips/TipsStatistic.jsx
+++ client/src/components/Tips/TipsStatistic.jsx
@@ -1,33 +1,115 @@
 import React from 'react';
-
-export default class TipsStatistic extends React.Component {
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+export class TipsStatistic extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      location: ''
     };
+    this.getAverageSalary = this.getAverageSalary.bind(this);
   }
 
+  componentDidMount () {
+    const set = this.setState.bind(this);
+    if(this.props.user) {
+      this.getActiveRoleLocation((data) => {
+        console.log(data, 'DATA GIVEN')
+        set({location: data});
+        this.getAverageSalary(data.split(',').map(l => l.trim()), (data) => {
+          this.setState({ avgSalary: data })
+        });
+      });
+    }
+  }
+
+
+  getActiveRoleLocation(callback) {
+    axios.get(`/api/applications?role_id=${this.props.user.active_role[0].id}`).then( res => {
+      console.log(res.data);
+      callback(`${res.data[0].city}, ${res.data[0].state}`)
+      return;
+    });
+  }
+
+  getAverageSalary(loc, data) {
+    const user = this.props.user;
+    axios.get(`/api/search?role=${user.active_role[0].name}&company=${user.active_role[0].company.name}&city=${loc[0]}&state=${loc[1]}`).then(res => {
+      this.setState({results: res.data})
+    })
+  }
+
+  placeCommasOnSalary(salary) {
+    let results = '$';
+    salary = salary.toString().split('.');
+    while(salary[0].length) {
+      results += salary[0].substring(0, 3);
+      salary[0] = salary[0].substring(3);
+
+      if(salary[0].length) {
+        results += ',';
+      }
+    }
+    results += '.00';
+    return results;
+  }
   render() {
-    return (
-      <div>
-        According to Glassdoor, the average Software Engineer salary in the U.S.
-        makes an average salary of $92,790.
-        <br />
-        Your job as a
-        <b>
-          Software Engineer
-        </b>
-        in
-        <b>
-          San Francisco, CA
-        </b>
-        has an average market value of:
-        <b>
-          $111,885.
-        </b>
-        <br />
-        Based on this data, here is what we suggest:
+    console.log('Damn girl, ', this.props, this.state);
+    let user = this.props.user;
+
+    if(user === undefined) {
+      return(
+        <div>
+          <Link to="/login">
+            Login
+          </Link> to find out more!
+        </div>
+      );
+    } else if(user && !user.active_role) {
+      return (
+        <div>
+        You have to first tell us where you work, go to
+        {' '}
+          <Link to="/applications">
+             applications
+          </Link> make an application and then update it on your user card in the homepage!
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+            Your job as a
+          <b>
+              {` ${user.active_role[0].name} `}
+          </b>
+            in
+          <b>
+              {` ${this.state.location} `}
+          </b>
+            has an average market value range of:
+          <b>
+            {this.state.results ? `${this.placeCommasOnSalary(Number(this.state.results.avgSalary * .93).toFixed(0))} - ${this.placeCommasOnSalary(Number(this.state.results.avgSalary * 1.07).toFixed(0))}`: undefined}
+          </b>
+            <br />
+            Based on this data, here is what we suggest:
       </div>
-    );
+      );
+    }
   }
 }
+
+const mapStateToProps = (state) => {
+  return {user: state.user.user}
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+
+  }, dispatch);
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(TipsStatistic);
