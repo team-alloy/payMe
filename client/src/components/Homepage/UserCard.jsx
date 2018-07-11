ec24b39a7c8af7496932759d1407c20a7592b897
Kenny Le <kennyle37@gmail.com>
2018-07-11 16:25:28 -0700

    ESLint code and add documentation

--- client/src/components/Homepage/UserCard.jsx
+++ client/src/components/Homepage/UserCard.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import axios from 'axios';
-
 import UserCardForm from './UserCardForm';
 
 export default class UserCard extends React.Component {
@@ -8,69 +6,10 @@ export default class UserCard extends React.Component {
     super(props);
     this.state = {
     };
-    // this.handleUserCardUpdate = this.handleUserCardUpdate.bind(this);
-    // this.handleGetAppliedRoles = this.handleGetAppliedRoles.bind(this);
-    // this.handleGetUserInformation = this.handleGetUserInformation.bind(this);
-    // this.componentDidMount = this.componentDidMount.bind(this);
   }
 
-  // // this function will change the state to form display
-  // handleFormViewSwitch() {
-  //   const { display } = this.state;
-  //   this.setState({ display });
-  // }
-
-  // handleViewChecker() {
-  //   const { display } = this.state;
-  //   if (display === true) {
-  //     return (
-  //       <div>
-  //         <UserCardForm {...this.props} />
-  //       </div>
-  //     );
-  //   }
-  //   return null;
-  // }
-
-  // componentDidMount() {
-  //   this.handleGetUserInformation((res) => {
-  //     this.setState({ currentUser: res })
-  //   })
-  //   this.handleGetAppliedRoles((res) => {
-  //     this.setState({ currentRoles: res })
-  //   })
-  // }
-
-  // handleGetAppliedRoles(callback) {
-  //   const { id } = this.props.session.user;
-  //   axios.get(`/api/roles?user_id=${id}`).then((res) => {
-  //     callback(res.data);
-  //   })
-  //     .catch(err => console.error(err));
-  // }
-
-  // handleGetUserInformation(callback) {
-  //   const { id } = this.props.session.user;
-  //   axios.get(`api/user?id=${id}`).then((res) => {
-  //     callback(res.data);
-  //   })
-  //     .catch(err => console.error(err));
-  // }
-
-  // handleUserCardUpdate(id, query) {
-  //   axios.patch((`/api/user?id=${id}`), query)
-  //     .then((res) => {
-  //       this.handleGetAppliedRoles((data) => {
-  //         this.setState({ currentRoles: data });
-  //       });
-  //     })
-  //     .then((res) => {
-  //       this.handleGetUserInformation((data) => {
-  //         this.setState({ currentUser: data });
-  //       });
-  //     });
-  // }
-
+  // Iterate through all the available roles
+  // and display the current role + company onto the user's profile
   handleDisplayRoles() {
     let temp = '';
 
@@ -89,45 +28,43 @@ export default class UserCard extends React.Component {
         <div />
       );
     }
-    console.log('NEGA' ,this.props)
-    console.log('ahhhhHHHH',user)
     return (
       <div className="ui teal card">
-       <div className="image">
-         <img src={user.profile_pic} alt=""/>
-       </div>
-       <div className="content">
-         <a>
-           {`${user.first_name} ${user.last_name}`}
-         </a>
-         <div className="meta">
-           <span className="description">
-             {this.props.roles ? this.handleDisplayRoles() : undefined}
-           </span>
-         </div>
-       </div>
-       <div className="extra content">
-         <a>
-           <div data-tooltip="Email">
-             <i className="envelope icon" />
-             {user.email}
-           </div>
-           <br />
-         </a>
-         <a>
-           <div data-tooltip="Current Salary">
-             <i className="dollar sign icon" />
-             {user.current_salary}
-           </div>
-         </a>
-         <a>
-           <div>
-             <UserCardForm
-               update={this.props.update} user={this.props.user} roles={this.props.roles}
-             />
-           </div>
-         </a>
-       </div>
+        <div className="image">
+          <img src={user.profile_pic} alt="" />
+        </div>
+        <div className="content">
+          <a>
+            {`${user.first_name} ${user.last_name}`}
+          </a>
+          <div className="meta">
+            <span className="description">
+              {this.props.roles ? this.handleDisplayRoles() : undefined}
+            </span>
+          </div>
+        </div>
+        <div className="extra content">
+          <a>
+            <div data-tooltip="Email">
+              <i className="envelope icon" />
+              {user.email}
+            </div>
+            <br />
+          </a>
+          <a>
+            <div data-tooltip="Current Salary">
+              <i className="dollar sign icon" />
+              {user.current_salary}
+            </div>
+          </a>
+          <a>
+            <div>
+              <UserCardForm
+                update={this.props.update} user={this.props.user} roles={this.props.roles}
+              />
+            </div>
+          </a>
+        </div>
       </div>
     );
   }
